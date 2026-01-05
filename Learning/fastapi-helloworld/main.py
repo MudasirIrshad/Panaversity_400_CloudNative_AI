d494ef4964c024df142e4a6e1cb74aad479a1727
Mudasir Irshad <mudasirirshad47@gmail.com>
2026-01-05 11:43:35 +0500

    understanding fastapi

---
+++ Learning/fastapi-helloworld/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/todo")
+def todo()-> list[dict[str,int | str]]:
+    return [
+        {
+            "id": 1,
+            "task": "Implement FastAPI application"
+        },
+        {
+            "id": 2,
+            "task": "Create endpoints for CRUD operations"
+        }
+    ]
