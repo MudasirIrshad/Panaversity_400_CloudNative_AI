2762a61f9c51ab3d3e2adfbdfbf26980ab8582d5
Mudasir Irshad <mudasirirshad47@gmail.com>
2026-02-22 13:47:23 +0500

    docker multistage build for kubernetes

---
+++ Learning/Docker/11_kubernetes_hello_search/main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Depends, HTTPException
+import uvicorn
+from typing import List
+from models import Task, engine
+from sqlmodel import Session, select
+
+app = FastAPI()
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/")
+def root_file():
+    return {"message": "Welcome to the Task Management API Mudasir Irshad!"}
+
+@app.post("/create_task")
+def create_task(task: Task, session: Session = Depends(get_session)):
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return {"Following task is created: ": task}
+
+@app.get("/get_tasks")
+def read_tasks( session: Session = Depends(get_session)):
+    tasks = session.exec(select(Task)).all()
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def read_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_update: Task, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    update_data = task_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(task, key, value)
+
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    session.delete(task)
+    session.commit()
+    return {"message": "Task deleted successfully"}
